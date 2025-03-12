749d96d8414f7522ec393a92aefb079a16dadf14
youssefali123 <77068196+youssefali123@users.noreply.github.com>
2025-03-12 02:26:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -381,4 +381,8 @@ console.log(string.match(reg));
 
 // for scrolling
 
-
+document.addEventListener('touchmove', function(event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+}, { passive: false });
